170d2b8d866a10b0112b47661714102d3b523589
saokiritoni <soeun.love.lee@gmail.com>
2025-07-05 21:26:00 +0900

    [init] #1 init settings

---
+++ src/main/java/DGU_AI_LAB/admin_be/AdminBeApplication.java
@@ -0,0 +1,13 @@
+package DGU_AI_LAB.admin_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DGU_AI_LAB/admin_be/AdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package DGU_AI_LAB.admin_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
