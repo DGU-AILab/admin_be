256219c26ef6835f978c494795c20f53842b5cbd
saokiritoni <soeun.love.lee@gmail.com>
2025-07-07 15:50:47 +0900

    [feat] #7 CorsConfig 구현

---
+++ src/main/java/DGU_AI_LAB/admin_be/global/config/CorsConfig.java
@@ -0,0 +1,46 @@
+package DGU_AI_LAB.admin_be.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${deployment.frontend.production}")
+    private String frontendProduction;
+
+    @Value("${deployment.frontend.local}")
+    private String frontendLocal;
+
+    @Value("${deployment.backend.production}")
+    private String backendProduction;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        // CORS 허용할 URL
+        List<String> allowedOrigins = Arrays.asList(
+                frontendProduction,
+                frontendLocal,
+                backendProduction
+        );
+        config.setAllowedOrigins(allowedOrigins);
+
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
