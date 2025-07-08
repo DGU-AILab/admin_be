901fb06d4ac8afb637391bf814aac202d618c491
aapdo <jade04342@gmail.com>
2025-07-08 22:41:32 +0900

    [feat] #16 smtp를 사용할 수 있는 sendMailAlert, Slack에 알람을 보낼 수 있는 sendSlackAlert 개발 완료

--- src/main/java/DGU_AI_LAB/admin_be/domain/alarm/service/AlarmService.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/alarm/service/AlarmService.java
@@ -8,6 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import java.util.Map;
 
@@ -15,23 +17,43 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AlarmService {
 
-    @Value("${slack.webhook-url}")
-    private String webhookUrl;
+    @Value("${slack-webhook-url.monitoring}")
+    private String defaultWebhookUrl;
     private final RestTemplate restTemplate = new RestTemplate();
-    public void sendAlert(String message) {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendSlackAlert(String message) {
+        sendSlackAlert(message, null);
+    }
+
+    public void sendSlackAlert(String message, String webhookUrl) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> payload = Map.of("text", message);
-
         HttpEntity<Map<String, String>> request = new HttpEntity<>(payload, headers);
 
-        ResponseEntity<String> response = restTemplate.postForEntity(webhookUrl, request, String.class);
+        String urlToUse = (webhookUrl != null && !webhookUrl.isEmpty()) ? webhookUrl : defaultWebhookUrl;
+
+        ResponseEntity<String> response = restTemplate.postForEntity(urlToUse, request, String.class);
 
         if (!response.getStatusCode().is2xxSuccessful()) {
             System.err.println("Slack 알림 전송 실패: " + response.getStatusCode());
         } else {
             System.out.println("Slack 알림 전송 성공");
         }
+    }
+    public void sendMailAlert(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+        System.out.printf("메일 전송 성공: 수신자=%s, 제목=%s%n", to, subject);
     }
 }
