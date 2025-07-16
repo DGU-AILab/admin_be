a7bb1c10711ff59c85d4414d7ee775506b0c7699
saokiritoni <soeun.love.lee@gmail.com>
2025-07-16 12:21:06 +0900

    [refactor] #30 필요한 repository 생성 및 annotation 추가

--- src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/ResourceGroupRepository.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/ResourceGroupRepository.java
@@ -1,10 +1,9 @@
 package DGU_AI_LAB.admin_be.domain.users.repository;
 
-import DGU_AI_LAB.admin_be.domain.users.entity.UsedId;
+import DGU_AI_LAB.admin_be.domain.users.entity.ResourceGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsedIdRepository extends JpaRepository<UsedId,Long> {
-    boolean existsById(Long id);
+public interface ResourceGroupRepository extends JpaRepository<ResourceGroup, Long> {
 }
--- src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/UserRepository.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package DGU_AI_LAB.admin_be.domain.users.repository;
 
-
 import DGU_AI_LAB.admin_be.domain.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByUsernameAndIsActiveTrue(String username);
