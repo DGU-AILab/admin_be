57d3f3b581a00d561c2b5328fc9e2f2b666cddc2
chaehyeo-n <parkche03@naver.com>
2025-07-19 18:58:27 +0900

    [feat] #34 예외처리

--- src/main/java/DGU_AI_LAB/admin_be/domain/requests/service/RequestService.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/requests/service/RequestService.java
@@ -3,12 +3,12 @@ package DGU_AI_LAB.admin_be.domain.requests.service;
 import DGU_AI_LAB.admin_be.domain.requests.dto.request.SaveRequestDTO;
 import DGU_AI_LAB.admin_be.domain.requests.entity.Request;
 import DGU_AI_LAB.admin_be.domain.requests.repository.RequestRepository;
+import DGU_AI_LAB.admin_be.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import DGU_AI_LAB.admin_be.error.ErrorCode;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +17,10 @@ public class RequestService {
 
     @Transactional
     public SaveRequestDTO saveRequest(SaveRequestDTO UseRequest) {
+        if (UseRequest.answers() == null || UseRequest.answers().isEmpty()) {
+            throw new BusinessException(ErrorCode.MISSING_REQUEST_PARAMETER);
+        }
+
         Request saved = requestRepository.save(UseRequest.toEntity());
         return SaveRequestDTO.fromEntity(saved);
     }
@@ -29,7 +33,7 @@ public class RequestService {
 
     public SaveRequestDTO getRequestById(Long id) {
         Request request = requestRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Request not found with id " + id));
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_NOT_FOUND));
         return SaveRequestDTO.fromEntity(request);
     }
 }
