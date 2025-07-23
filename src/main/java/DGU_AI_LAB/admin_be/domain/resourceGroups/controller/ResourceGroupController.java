51c4e65c3f616996a1e3cf8e80bc2f55ceeb0c0d
saokiritoni <soeun.love.lee@gmail.com>
2025-07-23 18:07:03 +0900

    [feat] #40 ResourceGroup 조회

--- src/main/java/DGU_AI_LAB/admin_be/domain/resourceGroups/controller/ResourceGroupController.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/resourceGroups/controller/ResourceGroupController.java
@@ -1,12 +1,15 @@
 package DGU_AI_LAB.admin_be.domain.resourceGroups.controller;
 
 import DGU_AI_LAB.admin_be.domain.resourceGroups.dto.ResourceGroupCreateRequest;
+import DGU_AI_LAB.admin_be.domain.resourceGroups.dto.ResourceGroupResponse;
 import DGU_AI_LAB.admin_be.domain.resourceGroups.service.ResourceGroupService;
 import DGU_AI_LAB.admin_be.global.common.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class ResourceGroupController {
     public ResponseEntity<?> createResourceGroup(@RequestBody @Valid ResourceGroupCreateRequest request) {
         resourceGroupService.createResourceGroup(request);
         return SuccessResponse.created(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllResourceGroups() {
+        List<ResourceGroupResponse> response = resourceGroupService.getAllResourceGroups();
+        return SuccessResponse.ok(response);
     }
 }
--- src/main/java/DGU_AI_LAB/admin_be/domain/resourceGroups/service/ResourceGroupService.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/resourceGroups/service/ResourceGroupService.java
@@ -1,9 +1,12 @@
 package DGU_AI_LAB.admin_be.domain.resourceGroups.service;
 
 import DGU_AI_LAB.admin_be.domain.resourceGroups.dto.ResourceGroupCreateRequest;
+import DGU_AI_LAB.admin_be.domain.resourceGroups.dto.ResourceGroupResponse;
 import DGU_AI_LAB.admin_be.domain.resourceGroups.repository.ResourceGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,12 @@ public class ResourceGroupService {
 
     public void createResourceGroup(ResourceGroupCreateRequest request) {
         resourceGroupRepository.save(request.toEntity());
+    }
+
+    public List<ResourceGroupResponse> getAllResourceGroups() {
+        return resourceGroupRepository.findAll()
+                .stream()
+                .map(ResourceGroupResponse::fromEntity)
+                .toList();
     }
 }
