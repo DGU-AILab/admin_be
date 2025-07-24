a32249e47debf3108ae9a4629a7ff0b99dad6f40
saokiritoni <soeun.love.lee@gmail.com>
2025-07-24 12:09:26 +0900

    [fix] #42 토큰 필요없는 경로 추가

--- src/main/java/DGU_AI_LAB/admin_be/global/auth/SecurityWhitelist.java
+++ src/main/java/DGU_AI_LAB/admin_be/global/auth/SecurityWhitelist.java
@@ -8,7 +8,8 @@ public class SecurityWhitelist {
             "/", "/swagger/**", "/swagger-ui/**", "/v3/api-docs/**",
             "/api/auth/token/**", "/api/auth/login", "/api/auth/reissue",
             "/auth/callback/**", "/api/auth/register",
-            "/actuator/health", "/actuator/info"
+            "/actuator/health", "/actuator/info",
+            "/api/resources/**", "api/requests/**" // 제거 필요
     };
 
     public static final List<String> EXACT_SKIP_PATHS = List.of(
@@ -18,6 +19,8 @@ public class SecurityWhitelist {
 
     public static final List<String> PATTERN_SKIP_PATHS = List.of(
             // 여기에 토큰 필요없는 경로 작성
+            "/api/resources/**", // 제거 필요
+            "api/requests/**"
     );
 }
 
